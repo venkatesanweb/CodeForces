5f3d3de9747b42db245dc66a380286141023e063
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2025-12-07 21:43:32 +0530

    Add files via upload

---
+++ Minimize!.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        cout<<(a-a)+(b-a)<<endl;
+    }
+    
+    return 0;
+}
